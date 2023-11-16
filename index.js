c7adea77b24169a969289686b0e4b8503553b6e8
Ansik <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2023-11-16 21:36:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+function dateBlock(dob){
+    let dob_yr = dob.split("-")[0];
+    let curr_yr = new Date().getFullYear();
+
+    let diff = (curr_yr-dob_yr);
+
+    if(diff < 18 || diff > 55){
+        alert("Age must be in between 18 and 55 years !");
+        return false;
+    }
+
+    return true;
+};
+
 let userForm = document.getElementById("user-form");
+
 let userEntries = [];
 
 const retrieveEntries = () => {
@@ -12,63 +27,62 @@ const retrieveEntries = () => {
     return entries;
 }
 
-userEntries = retrieveEntries();
 
 const displayEntries = () => {
     const entries = retrieveEntries();
-
+    
     const tableEntries = entries.map((entry) => {
         const nameCell = `<td class = 'border px-4 py-2'>${entry.name}</td>`;
         const emailCell = `<td class = 'border px-4 py-2'>${entry.email}</td>`;
         const passwordCell = `<td class = 'border px-4 py-2'>${entry.password}</td>`;
         const dobCell = `<td class = 'border px-4 py-2'>${entry.dob}</td>`;
         const termsandConditionsCell = `<td class = 'border px-4 py-2'>${entry.acceptedTermsandConditions}</td>`;
-
+        
         const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${termsandConditionsCell}</tr>`;
         return row;
     }).join('\n');
-
+    
     const table = `<table class = 'table-auto w-full'<tr>
-     <th class = 'px-4 py-2'> <u> Name </u> </th>
-     <th class = 'px-4 py-2'> <u> Email </u> </th>
-     <th class = 'px-4 py-2'> <u> Password </u> </th>
-     <th class = 'px-4 py-2'> <u> dob </u> </th>
-     <th class = 'px-4 py-2'> <u> accepted terms? </u> </th>
-     </tr>${tableEntries} </table>`;
-
+    <th class = 'px-4 py-2'> <u> Name </u> </th>
+    <th class = 'px-4 py-2'> <u> Email </u> </th>
+    <th class = 'px-4 py-2'> <u> Password </u> </th>
+    <th class = 'px-4 py-2'> <u> dob </u> </th>
+    <th class = 'px-4 py-2'> <u> accepted terms? </u> </th>
+    </tr>${tableEntries} </table>`;
+    
     let details = document.getElementById("user-entries");
     details.innerHTML = table;
 }
+
 const saveUserForm = (event) => {
-
+    
     event.preventDefault();
-
+    
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const dob = document.getElementById("dob").value;
-    const acceptedTermsandConditions = document.getElementById("acceptTerms").value;
-
-    const entry = {
-        name,
-        email,
-        password,
-        dob,
-        acceptedTermsandConditions
-    }
+    const acceptedTermsandConditions = document.getElementById("acceptTerms").checked;
     
-    let date = dob.split("-");
-
-    let year = Number(date[0]);
-
-    if(year < 2005 && year > 1968){
+    if(dateBlock(dob)){
+        const entry = {
+            name,
+            email,
+            password,
+            dob,
+            acceptedTermsandConditions
+        }
+        
+        
+        userEntries = retrieveEntries();
         userEntries.push(entry);
+        
+        localStorage.setItem("user-entries",JSON.stringify(userEntries));
     }
     
-    localStorage.setItem("user-entries",JSON.stringify(userEntries));
     displayEntries();
+    userForm.reset();
 }
-
 
 userForm.addEventListener("submit",saveUserForm);
 displayEntries();
