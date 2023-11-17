e07527b9cc2f86ec377d11b12c23bfa3bcdb1d9e
Ansik <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2023-11-17 20:55:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,9 +40,7 @@ const displayEntries = () => {
 }
 
 const saveUserForm = (event) => {
-    
-    event.preventDefault();
-    
+        
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
